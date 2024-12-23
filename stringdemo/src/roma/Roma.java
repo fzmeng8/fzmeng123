037e63e2589186ba8f6c828d8e62f2b00795a21e
fzmeng <fzmeng@bupt.edu.cn>
2024-12-23 20:17:36 +0800

    roma exercise

---
+++ stringdemo/src/roma/Roma.java
@@ -0,0 +1,24 @@
+package roma;
+import java.util.Scanner;
+public class Roma {
+    public static boolean isNumeric(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+//      键盘录入
+        String str = "";
+
+    }
+}
