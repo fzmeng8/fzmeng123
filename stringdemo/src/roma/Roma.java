47c45db78fff58c51abe6c6d444ef7ccbe4fa696
fzmeng <fzmeng@bupt.edu.cn>
2024-12-23 20:40:34 +0800

    roma exercise

--- stringdemo/src/roma/Roma.java
+++ stringdemo/src/roma/Roma.java
@@ -2,13 +2,30 @@ package roma;
 import java.util.Scanner;
 public class Roma {
     public static boolean isNumeric(String str) {
+        if (str == null || str.length() != 9) {
+            return false;
+        }
+
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
         }
         return true;
     }
 
 
+    public static void arabToRoman(String str) {
+        char[] RomanArr = {' ','Ⅰ','Ⅱ','Ⅲ','Ⅳ','Ⅴ','Ⅵ','Ⅶ','Ⅷ','Ⅸ'};
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            int num = c - '0';
+            roman.append(RomanArr[num]);
+        }
+        System.out.println(roman);
+    }
 
 
 
@@ -18,7 +35,19 @@ public class Roma {
 
     public static void main(String[] args) {
 //      键盘录入
-        String str = "";
+        System.out.println("input a 9 wei number: ");
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        boolean isNum = isNumeric(str);
+        if (isNum) {
+            System.out.println(str + " is a number.");
+        } else {
+            System.out.println(str + " is not a number.");
+        }
+
+        arabToRoman(str);
+
 
     }
 }
