03abd9c955578a24887ce5ae6025e54a4f5e324e
fzmeng <fzmeng@bupt.edu.cn>
2024-12-25 20:54:04 +0800

    Arr1

--- stringdemo/src/ArrList/ArrListDemo1.java
+++ stringdemo/src/ArrList/ArrListDemo1.java
@@ -1,4 +1,23 @@
 package ArrList;
-
+import java.util.ArrayList;
 public class ArrListDemo1 {
+    public static void main(String[] args) {
+        ArrayList<String> sites = new ArrayList<>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Weibo");
+        sites.remove(3); // 删除第四个元素
+        sites.remove("Google");
+        sites.set(1, "Apple");
+        sites.get(1);
+        System.out.println(sites);
+        for (int i = 0; i < sites.size(); i++) {
+            System.out.println(sites.get(i));
+        }
+
+        for (String i : sites) {
+            System.out.println(i);
+        }
+    }
 }
